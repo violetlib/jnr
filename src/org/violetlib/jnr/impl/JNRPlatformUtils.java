e59937d7ee3fd053a885ffc8dde7d5ed05b0a948
Alan Snyder <ranchlens@cbfiddle.com>
2021-08-04 12:38:32 -0700

    Update macOS version parsing to handle macOS 11+.

--- src/org/violetlib/jnr/impl/JNRPlatformUtils.java
+++ src/org/violetlib/jnr/impl/JNRPlatformUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 Alan Snyder.
+ * Copyright (c) 2015-2021 Alan Snyder.
  * All rights reserved.
  *
  * You may not use, copy or modify this file, except in compliance with the license agreement. For details see
@@ -10,6 +10,7 @@ package org.violetlib.jnr.impl;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 import org.jetbrains.annotations.*;
@@ -34,28 +35,10 @@ public class JNRPlatformUtils
         if (platformVersionString == null) {
             String osName = System.getProperty("os.name");
             String osVersion = System.getProperty("os.version");
-
             boolean isOSX = osName.equals("Mac OS X") || osName.equals("macOS");
             if (isOSX) {
-
                 platformVersionString = osVersion;
-
-                int n1 = 0;
-                int n2 = 0;
-                int n3 = 0;
-
-                StringTokenizer st = new StringTokenizer(osVersion, ".");
-                if (st.hasMoreTokens()) {
-                    n1 = parseComponent(st.nextToken());
-                    if (st.hasMoreTokens()) {
-                        n2 = parseComponent(st.nextToken());
-                        if (st.hasMoreTokens()) {
-                            n3 = parseComponent(st.nextToken());
-                        }
-                    }
-                }
-
-                platformVersion = n1 * 10000 + n2 * 100 + n3;
+                platformVersion = obtainSystemVersion(osVersion);
                 return platformVersion;
             }
         }
@@ -63,16 +46,73 @@ public class JNRPlatformUtils
         return platformVersion;
     }
 
-    private static int parseComponent(@NotNull String s)
+    private static int obtainSystemVersion(@NotNull String s)
     {
-        try {
-            int n = Integer.parseInt(s);
-            if (n > 0) {
-                return n;
+        int version = parseSystemVersion(s);
+        if (version / 100 == 1016) {
+            // Liar, there is no macOS 10.16!
+            int v = readSystemVersion();
+            if (v > 0) {
+                return v;
             }
-        } catch (NumberFormatException ex) {
+        }
+        return version;
+    }
+
+    private static int readSystemVersion()
+    {
+        String[] cmd = { "/usr/bin/sw_vers", "-productVersion" };
+        String[] env = { "SYSTEM_VERSION_COMPAT=0" };
+        String s = command("sw_vers", cmd, env);
+        if (s != null) {
+            return parseSystemVersion(s);
         }
         return 0;
+    }
+
+    private static @Nullable String command(@NotNull String name, String @NotNull [] command, String @Nullable [] env)
+    {
+        try {
+            Process p = Runtime.getRuntime().exec(command, env);
+            InputStreamReader stdout = new InputStreamReader(p.getInputStream());
+            StringBuffer sb = new StringBuffer();
+            while (true) {
+                int n = stdout.read();
+                if (n == -1 || n == 10) {
+                    break;
+                }
+                sb.append((char) n);
+            }
+            return sb.toString();
+        } catch (Throwable th) {
+            System.err.println("Unable to run " + name + ": " + th);
+            return null;
+        }
+    }
+
+    private static int parseSystemVersion(@NotNull String s)
+    {
+        int version = 0;
+        int count = 0;
+        StringTokenizer st = new StringTokenizer(s, ".");
+        while (st.hasMoreTokens()) {
+            ++count;
+            if (count > 3) {
+                break;
+            }
+            String t = st.nextToken();
+            try {
+                int n = Integer.parseInt(t);
+                if (n >= 0 && n < 100) {
+                    version = version * 100 + n;
+                } else {
+                    return 0;
+                }
+            } catch (NumberFormatException ex) {
+                return 0;
+            }
+        }
+        return count == 2 ? version * 100 : version;
     }
 
     public static @Nullable Graphics2D toGraphics2D(@NotNull Graphics g)
